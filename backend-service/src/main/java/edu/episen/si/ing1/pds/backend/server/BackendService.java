077eaab99834b5f26381403090cca475823d0920
RomeoC13 <romeo.chatel@gmail.com>
2021-01-29 12:38:14 +0100

    added BackendService class with a logger

---
+++ backend-service/src/main/java/edu/episen/si/ing1/pds/backend/server/BackendService.java
@@ -0,0 +1,12 @@
+package edu.episen.si.ing1.pds.backend.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BackendService {
+    private static final Logger logger = LoggerFactory.getLogger(BackendService.class.getName());
+
+    public static void main(String[] args) {
+        logger.info("BackEnd is working");
+    }
+}
