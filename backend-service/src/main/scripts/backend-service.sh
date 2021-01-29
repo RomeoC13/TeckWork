ca85f20b1999872afdf9a10cf6f05bb0281e1fb8
RomeoC13 <romeo.chatel@gmail.com>
2021-01-29 13:31:02 +0100

    Added a script to execute backend code (NOT WORKING FOR NOW)

---
+++ backend-service/src/main/scripts/backend-service.sh
@@ -0,0 +1,4 @@
+m2=${M2_REPO}
+clsspth=src/main/resources
+clsspth+=${clsspth}:${m2}/edu/episen/si/ing1/pds/backend-service/1.0-SNAPSHOT/backend-service-1.0-SNAPSHOT-jar-with-dependencies.jar
+exec java -cp ${clsspth} edu.episen.si.ing1.pds.backend.server.BackendService $*
