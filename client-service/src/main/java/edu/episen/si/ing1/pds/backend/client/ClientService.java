75602c37912694568c5c7bb22370c1945688a3e9
RomeoC13 <romeo.chatel@gmail.com>
2021-01-29 13:54:35 +0100

    Added a logger to ClientService.java

--- client-service/src/main/java/edu/episen/si/ing1/pds/backend/client/ClientService.java
+++ client-service/src/main/java/edu/episen/si/ing1/pds/backend/client/ClientService.java
@@ -1,7 +1,13 @@
 package edu.episen.si.ing1.pds.backend.client;
 
-public class ClientService {
-    public static void main(String[] args) {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+public class ClientService {
+    private static final Logger logger = LoggerFactory.getLogger(ClientService.class.getName());
+
+    public static void main(String[] args) {
+        logger.info("Client is working");
     }
+
 }
