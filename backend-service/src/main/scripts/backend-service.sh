6bc85e4f6158cd390de67435b80b7438dec01d72
yorgunmichael <yorgunchacha@yahoo.fr>
2021-02-03 19:20:35 +0100

    erase + in the script

--- backend-service/src/main/scripts/backend-service.sh
+++ backend-service/src/main/scripts/backend-service.sh
@@ -1,3 +1,3 @@
 m2=${M2_REPO}
-clsspth+=${clsspth}:${m2}/edu/episen/si/ing1/pds/backend-service/1.0-SNAPSHOT/backend-service-1.0-SNAPSHOT-jar-with-dependencies.jar
+clsspth=${clsspth}:${m2}/edu/episen/si/ing1/pds/backend-service/1.0-SNAPSHOT/backend-service-1.0-SNAPSHOT-jar-with-dependencies.jar
 exec java -cp ${clsspth} edu.episen.si.ing1.pds.backend.server.BackendService $*
