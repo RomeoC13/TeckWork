83be89b1637c99d291788d34828a45ebc93441b6
yorgunmichael <yorgunchacha@yahoo.fr>
2021-02-03 21:13:43 +0100

    Adding file creation of client-service.sh

---
+++ client-service/src/main/script/client-service.sh
@@ -0,0 +1,4 @@
+m2=${M2_REPO}
+clsspth=${clsspth}:${m2}/edu/episen/si/ing1/pds/backend-service/1.0-SNAPSHOT/backend-service-1.0-SNAPSHOT-jar-with-dependencies.jar
+exec java -cp ${clsspth} edu.episen.si.ing1.pds.backend.client.ClientService $*
+
