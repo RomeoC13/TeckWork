85e8cb38834a7385dad50f4374c9c3f347d77d76
Alex POINOT <evoks94@gmail.com>
2021-02-19 17:20:35 +0100

    add of pool connection with postgresql

--- client-service/src/main/java/edu/episen/si/ing1/pds/backend/client/ClientService.java
+++ client-service/src/main/java/edu/episen/si/ing1/pds/backend/client/ClientService.java
@@ -1,6 +1,5 @@
 package edu.episen.si.ing1.pds.backend.client;
 
-import org.apache.commons.cli.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,39 +7,7 @@ public class ClientService {
     private static final Logger logger = LoggerFactory.getLogger(ClientService.class.getName());
 
     public static void main(String[] args) {
-        boolean isInTestMode = false;
-        int maxCo = 10;
-
-        Options options = new Options();
-        options.addOption("testMode", false, "a simple cli option to run this service as test, default is false");
-
-        final Option maxConnection = Option.builder().longOpt("maxConnections")
-                .hasArg()
-                .argName("maxConnections")
-                .desc("specify max connections in this run, default set to 10")
-                .build();
-
-        options.addOption(maxConnection);
-
-        CommandLineParser parser = new DefaultParser();
-        CommandLine cmd = null;
-        try {
-            cmd = parser.parse(options, args);
-        } catch (ParseException e) {
-            logger.error("Options parser error : " + e.getMessage());
-        }
-
-        String loggerInfo = "ClientService is running.";
-        if (cmd.hasOption("testMode")) {
-            isInTestMode = true;
-            loggerInfo += "\n Test mode is on";
-        }
-        if (cmd.hasOption("maxConnections")) {
-            maxCo = Integer.parseInt(cmd.getOptionValue("maxConnections"));
-            loggerInfo += "\n Max connections has been set to " + maxCo;
-
-        }
-        logger.info(loggerInfo);
+        logger.info("Client is working");
     }
 
 }
--- client-service/src/main/scripts/client-service.sh
+++ client-service/src/main/scripts/client-service.sh
@@ -1,4 +1,3 @@
-m2=${M2_REPO}
-clsspth=${clsspth}:${m2}/edu/episen/si/ing1/pds/backend-service/1.0-SNAPSHOT/backend-service-1.0-SNAPSHOT-jar-with-dependencies.jar
-exec java -cp ${clsspth} edu.episen.si.ing1.pds.backend.client.ClientService $*
-
+echo ${M2_HOME}
+classpth="${M2_HOME}\\edu\\episen\\si\\ing1\\pds\\backend-service\\1.0-SNAPSHOT\\backend-service-1.0-SNAPSHOT-jar-with-dependencies.jar"
+exec java -cp ${classpth} edu.episen.si.ing1.pds.backend.server.BackendService $*
