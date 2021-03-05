10711c2e2caa6908ee8f98075a2f084d29148d48
el-hadj <elhadj.balde@etu.u-pec.fr>
2021-03-05 11:38:37 +0100

    adding a CRUD menu for the pool connection

--- backend-service/src/main/java/edu/episen/si/ing1/pds/backend/server/DBConnection.java
+++ backend-service/src/main/java/edu/episen/si/ing1/pds/backend/server/DBConnection.java
@@ -5,11 +5,12 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.sql.SQLException;
 import java.util.Properties;
+import java.util.Scanner;
 
 
 public class DBConnection {
+	
     public static void main(String[] args) throws Exception{
         Connection conn = null;
 
@@ -17,40 +18,111 @@ public class DBConnection {
         try(FileInputStream fis = new FileInputStream("conf.properties")) {
             props.load(fis);
         }
-        System.out.println("jdbc.driver.class");
+        
 
         String url = props.getProperty("jdbc.url");
         String login = props.getProperty("jdbc.login");
         String pwd = props.getProperty("jdbc.password");
-        
-        try {
-            conn = DriverManager.getConnection(url, login, pwd);
-            if(conn != null) {
-               System.out.println("CONNECTION OK");
-            } else {
-                System.out.println("CONNECTION FAILED");
-            }
-
-            Statement statement = conn != null ? conn.createStatement() : null;
-
-
-            ResultSet rs = statement.executeQuery("SELECT * FROM USERS");
-
-            while(rs.next()) {
-                System.out.println(rs.getInt("USR_ID") + " " + rs.getString(2)  + " " + rs.getString("USR_PASSWORD"));
-            }
-        } catch (Exception e) {
-            if(conn != null) {
-                try {
-                    conn.close();
-                } catch (SQLException ex) {
-                    ex.printStackTrace();
-                }
-            }
+        try(Connection con = DriverManager.getConnection(url, login, pwd)){
+	        if(conn != null) {
+	          System.out.println("CONNECTION OK");
+	       }
         }
-
-
-
-
+        int choix = 0;
+    while(choix!= 4) {
+        	System.out.println("choose your number\n choose a number\n 1 - insert\n 2 - delete\n 3 - select\n ");
+        	Scanner sc = new Scanner(System.in);
+        	choix = sc.nextInt();
+        	switch (choix) {
+			case 1:
+				try(Connection con = DriverManager.getConnection(url, login, pwd)){
+					String request = "insert into users (name, login) values ('JE', 'first')";
+					try(Statement stm = con.createStatement()){
+						stm.executeUpdate(request);
+					}
+					
+				}
+				break;
+			case 2 :
+				try(Connection con = DriverManager.getConnection(url, login, pwd)) {
+					try(Statement stmt = con.createStatement()){
+						System.out.println("you want to delete an row\n ENTER AN ID ");
+						int id = sc.nextInt();
+						String request = "delete from users where id = "+id;
+						stmt.executeUpdate(request);
+					}
+					
+				} 
+				break;
+			case 3:
+				try(Connection con = DriverManager.getConnection(url, login, pwd)) {
+					String request  = "select * from users";
+					try(Statement stmt = con.createStatement();ResultSet result = stmt.executeQuery(request)){
+						while(result.next()) {
+							int id = result.getInt(1);
+							String name = result.getString(2);
+							String log = result.getString(3);
+							System.out.println(id+"-"+name+"-"+log);
+						}
+						
+					}
+					
+				}
+				break;
+			case 4 :
+				try(Connection con = DriverManager.getConnection(url, login, pwd)) {
+					System.out.println("please enter name");
+					String nameVar = sc.nextLine();
+					System.out.println(nameVar);
+					System.out.println("please enter your login");
+					String loginVar = sc.nextLine();
+					System.out.println(login);
+				}
+				break;
+						
+			default:
+				System.out.println("you selected wrong number");
+				break;
+			}
+        
+       
+    
+        }   
     }
-}
+    
+}    
+  
+
+        
+//        try {
+//            conn = DriverManager.getConnection(url, login, pwd);
+//            if(conn != null) {
+//               System.out.println("CONNECTION OK");
+//            } else {
+//                System.out.println("CONNECTION FAILED");
+//            }
+//
+//            //Statement statement = conn != null ? conn.createStatement() : null;
+//            Statement statement = conn.createStatement();
+//            
+//            
+//
+//
+//            ResultSet rs = statement.executeQuery("SELECT * FROM users");
+//
+//            while(rs.next()) {
+//                int idUsers = rs.getInt(2);
+//                System.out.println(idUsers);
+//            }
+//        } catch (Exception e) {
+//            if(conn != null) {
+//                try {
+//                    conn.close();
+//                } catch (SQLException ex) {
+//                    ex.printStackTrace();
+//                }
+//            }
+//        }
+//
+
+
