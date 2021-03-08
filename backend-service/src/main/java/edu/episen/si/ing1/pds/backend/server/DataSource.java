f468c873b623bf35b2af22f00e427195e839daeb
el-hadj <elhadj.balde@etu.u-pec.fr>
2021-03-08 16:50:15 +0100

    adding jDBCConnectionClass and DataSource classes

--- backend-service/src/main/java/edu/episen/si/ing1/pds/backend/server/DataSource.java
+++ backend-service/src/main/java/edu/episen/si/ing1/pds/backend/server/DataSource.java
@@ -1,5 +1,20 @@
 package edu.episen.si.ing1.pds.backend.server;
 
-public class DataSource {
+import java.sql.Connection;
 
+public class DataSource {
+	
+	private static JDBCConnectionPool pool;
+	
+	public static Connection sendBackData() {
+		return pool.sendBackConnection();
+	}
+	
+	public static void putBackData(Connection con) {
+		pool.putBackConnection(con);
+	}
+	
+	public static void closeData() {
+		pool.closeConnexion();
+	}
 }
