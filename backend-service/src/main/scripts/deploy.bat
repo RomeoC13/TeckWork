0ce065e1086a60634b3e92963d3dfb05201b0770
yorgunmichael <yorgunchacha@yahoo.fr>
2021-03-16 21:03:21 +0100

    creation of deployment script

---
+++ backend-service/src/main/scripts/deploy.bat
@@ -0,0 +1,3 @@
+scp C:/Users/yorgu/.m2/repository/edu/episen/si/ing1/pds/backend-service/1.0-SNAPSHOT/backend-service-1.0-SNAPSHOT-jar-with-dependencies.jar tata@172.31.254.99:/home/tata/server/
+scp C:\Users\yorgu\IdeaProjects\teck-work\backend-service\src\main\scripts\backend-service.sh tata@172.31.254.99:/home/tata/server/
+ssh tata@172.31.254.99 chmod 777 /home/tata/server/backend-service.sh
