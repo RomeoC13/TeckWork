7e2faaef0337ca1e6d12a59faeaf0ea9e8530917
Alex POINOT <evoks94@gmail.com>
2021-03-19 14:41:16 +0100

    modification on deploy.bat script

--- backend-service/src/main/scripts/deploy.bat
+++ backend-service/src/main/scripts/deploy.bat
@@ -1,3 +1,5 @@
-scp C:/Users/yorgu/.m2/repository/edu/episen/si/ing1/pds/backend-service/1.0-SNAPSHOT/backend-service-1.0-SNAPSHOT-jar-with-dependencies.jar tata@172.31.254.99:/home/tata/server/
-scp C:\Users\yorgu\IdeaProjects\teck-work\backend-service\src\main\scripts\backend-service.sh tata@172.31.254.99:/home/tata/server/
+set m2=%M2_REPO%
+set project=%PROJECT%
+scp %m2%/edu/episen/si/ing1/pds/backend-service/1.0-SNAPSHOT/backend-service-1.0-SNAPSHOT-jar-with-dependencies.jar tata@172.31.254.99:/home/tata/server/
+scp %project%\teck-work\backend-service\src\main\scripts\backend-service.sh tata@172.31.254.99:/home/tata/server/
 ssh tata@172.31.254.99 chmod 777 /home/tata/server/backend-service.sh
