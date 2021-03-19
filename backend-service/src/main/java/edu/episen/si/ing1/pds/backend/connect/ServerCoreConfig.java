64198b958ac00600daf669c7367265d1e882cb21
elhadj <elhadj.balde@etu.u-pec.fr>
2021-03-19 16:46:36 +0100

    connection to server client using a socket

---
+++ backend-service/src/main/java/edu/episen/si/ing1/pds/backend/connect/ServerCoreConfig.java
@@ -0,0 +1,35 @@
+package edu.episen.si.ing1.pds.backend.connect;
+
+public class ServerCoreConfig {
+	
+	public int  listenPort;
+	public int soTime;
+	public ServerCoreConfig(int listenPort, int soTime) {
+		super();
+		this.listenPort = listenPort;
+		this.soTime = soTime;
+	}
+	@Override
+	public String toString() {
+		return "ServerCireConfig ["
+				+ "listenPort=" + listenPort 
+				+ ", soTime=" + soTime
+							+ "]";
+	}
+	public int getListenPort() {
+		return listenPort;
+	}
+	public void setListenPort(int listenPort) {
+		this.listenPort = listenPort;
+	}
+	public int getSoTime() {
+		return soTime;
+	}
+	public void setSoTime(int soTime) {
+		this.soTime = soTime;
+	}
+	
+	
+	
+
+}
