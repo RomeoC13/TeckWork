155c3c71ccccd5cef10ab45fb17ba1eb49aa441e
RomeoC13 <romeo.chatel@gmail.com>
2021-03-24 15:56:27 +0100

    PropertiesClass is set to get all properties in BackendService

---
+++ backend-service/src/main/java/edu/episen/si/ing1/pds/backend/server/PropertiesClass.java
@@ -0,0 +1,34 @@
+package edu.episen.si.ing1.pds.backend.server;
+
+public class PropertiesClass {
+    private boolean testMode;
+    private Integer maxCo;
+    private String sqlReq;
+
+    public PropertiesClass() {
+    }
+
+    public boolean isTestMode() {
+        return testMode;
+    }
+
+    public void setTestMode(boolean testMode) {
+        this.testMode = testMode;
+    }
+
+    public Integer getMaxCo() {
+        return maxCo;
+    }
+
+    public void setMaxCo(Integer maxCo) {
+        this.maxCo = maxCo;
+    }
+
+    public String getSqlReq() {
+        return sqlReq;
+    }
+
+    public void setSqlReq(String sqlReq) {
+        this.sqlReq = sqlReq;
+    }
+}
