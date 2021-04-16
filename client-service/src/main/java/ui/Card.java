c2c6ffb1fed7b668489ab986a362e29cc136c684
RomeoC13 <romeo.chatel@gmail.com>
2021-04-16 14:42:25 +0200

    optimised Side_Menu

--- client-service/src/main/java/ui/Card.java
+++ client-service/src/main/java/ui/Card.java
@@ -4,21 +4,22 @@ import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
 
-public class Menu extends JFrame {
+public class Card extends JFrame {
 
-    public Menu() {
+    public Card() {
         this.setTitle("Welcome to techwork");
         this.setSize(800, 500);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Side_Menu sm= new Side_Menu();
         getContentPane().add(sm, BorderLayout.WEST);
+
         this.setVisible(true);
 
 
     }
 
     public static void main(String[] args) {
-        Menu f = new Menu();
+        Card f = new Card();
     }
 
 
---
+++ client-service/src/main/java/ui/Side_Menu.java
@@ -0,0 +1,32 @@
+package ui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Side_Menu extends JPanel {
+
+
+
+    public Side_Menu() {
+        JLabel label = new JLabel();
+        JButton buton1 = new JButton("Accueil");
+        JButton buton2 = new JButton("Indicateur");
+        JButton buton3 = new JButton("Mapping");
+        JButton buton4 = new JButton("Carte");
+
+
+        this.add(label);
+        this.add(buton1);
+        this.add(buton2);
+        this.add(buton3);
+        this.add(buton4);
+        this.setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
+        this.setAlignmentY(Component.CENTER_ALIGNMENT);
+        this.setAlignmentX(Component.CENTER_ALIGNMENT);
+        this.setBackground(new Color(68, 114, 196));
+        this.setPreferredSize(new Dimension(200, 100));
+
+        label.setFont(new Font("Serif", Font.CENTER_BASELINE, 20));
+        label.setText("<html><body><font color='blanc'>Entreprise: Tech-Work </body></html>");
+    }
+}
