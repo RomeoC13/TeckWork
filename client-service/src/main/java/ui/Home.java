326ba6745d0aa3544f6ae0bdf398fec2acccdddc
elhadj <elhadj.balde@etu.u-pec.fr>
2021-04-21 16:19:49 +0200

    finishing the entry page for option

--- client-service/src/main/java/ui/Home.java
+++ client-service/src/main/java/ui/Home.java
@@ -1,5 +1,10 @@
 package ui;
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -7,6 +12,7 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
@@ -20,25 +26,31 @@ public class Home {
 	
 	
 	public Home() {
+		
 		myFrame.setVisible(true);
 		myFrame.setTitle("page d'accueil");
-		myFrame.setSize(500,500);
+		myFrame.setSize(500,300);
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocationRelativeTo(null);
-		JPanel  panel = (JPanel)myFrame.getContentPane();
-		panel.setLayout(new BorderLayout());
+		
+		JPanel  contentPane = (JPanel)myFrame.getContentPane();
+		contentPane.setLayout(new GridLayout(3,1));
+		
 		
 		combox = new JComboBox(element);
-		combox.setMaximumSize(new Dimension(20,20));
-		panel.add(BorderLayout.CENTER,combox);
+		combox.setPreferredSize(new Dimension(70,70));
 		
-		JLabel label = new JLabel("veuillez entrer l'element que vous souhaiter");
-		//setSize(50,30);
-		panel.add(BorderLayout.NORTH, label);
-		
-		JButton button = new JButton("Valider");
-		button.setSize(50, 50);
-		button.addActionListener(new ActionListener() {
+		contentPane.add( topOptionPanel());
+		contentPane.add(combox);
+		contentPane.add(southOptionPanel());
+
+	}
+	
+	private JPanel southOptionPanel() {
+		JPanel southPanel = new JPanel(new FlowLayout());
+		JButton ValidateButon = new JButton("Valider");
+		ValidateButon.setPreferredSize(new Dimension(100,70));;
+		ValidateButon.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -49,8 +61,32 @@ public class Home {
 					
 			}
 		});
-		panel.add(BorderLayout.SOUTH,button);
-
+		southPanel.add(ValidateButon);
+		
+		JButton cancelButon = new JButton("Annuler");
+		cancelButon.setPreferredSize(new Dimension(100,70));
+		cancelButon.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "Vous avez annulé la session");
+				myFrame.dispose();
+			}
+		});
+		southPanel.add(cancelButon);
+		southPanel.setBackground(Color.white);
+		
+		
+		return southPanel;
+	}
+	
+	private JPanel topOptionPanel () {
+		JPanel topPanel = new JPanel ();
+		JLabel topMessage = new JLabel("Veuillez choisir une option");
+		topMessage.setFont(new Font("Sylfaen", Font.CENTER_BASELINE, 20));
+		topPanel.setBackground(Color.white);
+		topPanel.add(topMessage);
+		return topPanel;
 	}
 
 	public static void main(String[] args) throws UnsupportedLookAndFeelException {
