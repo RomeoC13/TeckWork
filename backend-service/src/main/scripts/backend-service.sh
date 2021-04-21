645ae3b13df062d91320489a9ba9439af90f4837
elhadj <elhadj.balde@etu.u-pec.fr>
2021-04-21 04:24:36 +0200

    changing definetly the two script sh main client and server

--- backend-service/src/main/scripts/backend-service.sh
+++ backend-service/src/main/scripts/backend-service.sh
@@ -1,3 +1,5 @@
-echo ${M2_HOME}
-exec java -cp /home/tata/server/backend-service-1.0-SNAPSHOT-jar-with-dependencies.jar edu.episen.si.ing1.pds.backend.server.release2.Server $*
-#exec java -cp ${classpth} edu.episen.si.ing1.pds.backend.server.DBConnection $*
+PROJECT_PATH="${M2_REPO}/edu/episen/si/ing1/pds/backend-service/1.0-SNAPSHOT"
+JAR_FILE="backend-service-1.0-SNAPSHOT-jar-with-dependencies.jar"
+CLASS_PATH=${PROJECT_PATH}/${JAR_FILE}
+MAIN_CLASS="edu.episen.si.ing1.pds.backend.server.release2.Server"
+java -cp ${CLASS_PATH} ${MAIN_CLASS} $*
