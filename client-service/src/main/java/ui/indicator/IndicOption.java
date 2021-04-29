de49617f77a2a5065455739fd58f95aaebc3a7d7
elhadj <elhadj.balde@etu.u-pec.fr>
2021-04-29 16:43:20 +0200

    indicator option for calculating a data indicator

---
+++ client-service/src/main/java/ui/indicator/IndicOption.java
@@ -0,0 +1,17 @@
+package ui.indicator;
+
+import java.sql.Connection;
+
+public class IndicOption {
+	
+	
+	private double tauxCalcul () {
+		double capacity = 0.0;
+		
+		try {
+			Connection con = new 
+		}
+		return capacity;
+	}
+
+}
