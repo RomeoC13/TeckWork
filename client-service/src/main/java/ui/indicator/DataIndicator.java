a83ab4cd655360fcbc5064de723a5eedcca60938
elhadj <elhadj.balde@etu.u-pec.fr>
2021-06-21 16:40:32 +0200

    improving my UC indicator for each categories building, company and general information

--- client-service/src/main/java/ui/indicator/DataIndicator.java
+++ client-service/src/main/java/ui/indicator/DataIndicator.java
@@ -6,29 +6,15 @@ import client.Client;
 public class DataIndicator {
 
     public DataIndicator(){}
-    public String getOccupancy(){
-        String values = Client.getSend("rateOccupation");
-
-        return values;
-    }
+    public String getOccupancy(){return Client.getSend("rateOccupation"); }
     public String getConnectedObject(){
         return Client.getSend("connectedObject");
     }
     public String getEquipment(){
         return Client.getSend("allEquipment");
     }
-    public String getSensor(){
-        String values = Client.getSend("allSensor");
-        return values;
-    }
-    public String getCompany(){
-        String values = Client.getSend("allCompany");
-        return values;
-    }
-
-    public String energyConsumption(){
-        String values = Client.getSend("energyConsommation");
-        return values;
-    }
+    public String getSensor(){return Client.getSend("allSensor");}
+    public String getCompany(){return Client.getSend("allCompany"); }
+    public String energyConsumption(){return Client.getSend("energyConsommation"); }
 
 }
--- client-service/src/main/java/ui/indicator/Indicator.java
+++ client-service/src/main/java/ui/indicator/Indicator.java
@@ -24,6 +24,8 @@ public class Indicator extends JFrame {
 	JTextField sensorField = new JTextField();
 	JTextField companyField = new JTextField();
 	JTextField energyField = new JTextField();
+	JTextField floorField = new JTextField();
+	JTextField batimentField = new JTextField();
 	JPanel seconJPanel = new JPanel();
 	Side_Menu sm;
 	private JTextField textField;
@@ -31,6 +33,10 @@ public class Indicator extends JFrame {
 	private String company_name;
 	private DataIndicator data = new DataIndicator();
 	protected IndicOption option = new IndicOption();
+	protected JButton allInfo;
+	protected JButton infoByCompany;
+	protected JButton infoByBuilding;
+
 
 	public Indicator(String company_name) {
 		this.company_name = company_name;
@@ -40,7 +46,7 @@ public class Indicator extends JFrame {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		sm = new Side_Menu(company_name);
 		JPanel contentPane = (JPanel) this.getContentPane();
-		getContentPane().setLayout(new BorderLayout());
+		contentPane.setLayout(new BorderLayout());
 		contentPane.add(sm, BorderLayout.WEST);
 		contentPane.add(addingAllMenu(), BorderLayout.CENTER);
 
@@ -60,17 +66,21 @@ public class Indicator extends JFrame {
 
 		JPanel topPanel = new JPanel(new FlowLayout());
 		topPanel.setPreferredSize(new Dimension(10, 110));
-		JButton allInfo = new JButton("information generale");
+		allInfo = new JButton("information generale");
 		allInfo.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-			titleField.setText("vous êtes sur les informations génerales des batiments");
-				//occupationField.setText(data.getOccupancy());
-						/*itemsField.setText(data.getConnectedObject());
-						equipmentField.setText(data.getEquipment());*/
+
+				titleField.setText("vous êtes sur les informations génerales des batiments");
+				occupationField.setText(data.getOccupancy());
+				itemsField.setText(data.getConnectedObject());
+				equipmentField.setText(data.getEquipment());
 				sensorField.setText(data.getSensor());
 				companyField.setText(data.getCompany());
 				energyField.setText(data.energyConsumption());
+//				floorField.disable();
+//				batimentField.disable();
+
 			}
 		});
 		topPanel.add(allInfo);
@@ -79,7 +89,7 @@ public class Indicator extends JFrame {
 
 
 
-		JButton infoByCompany = new JButton("information par entreprise");
+		infoByCompany = new JButton("information par entreprise");
 		infoByCompany.addActionListener(new ActionListener() {
 
 			@Override
@@ -93,7 +103,7 @@ public class Indicator extends JFrame {
 					@Override
 					public void actionPerformed(ActionEvent e) {
 						field.setText(comboBox.getSelectedItem().toString());
-						titleField.setText("vous êtes sur les informations génerales de l'entreprise "+ field.getText());
+
 					}
 				});
 				for(Object o: test)
@@ -105,23 +115,34 @@ public class Indicator extends JFrame {
 
                 int response = JOptionPane.showConfirmDialog(null, pan, "choix d'une entreprise", JOptionPane.OK_CANCEL_OPTION);
 				if(response == JOptionPane.OK_OPTION){
-					Client.map.get("CompanyConnectedObject").put("company_name", field.getText());
-					itemsField.setText(option.objectCompany());
+					titleField.setText("vous êtes sur les informations génerales de l'entreprise : "+field.getText());
+//					Client.map.get("CompanyConnectedObject").put("company_name", field.getText());
+//					itemsField.setText(option.objectCompany());
 					Client.map.get("AllEquipmentCompany").put("company_name", field.getText());
 					equipmentField.setText(option.equipmentCompany());
 					Client.map.get("allSensorCompany").put("company_name", field.getText());
 					sensorField.setText(option.sensorCompany());
-					Client.map.get("energyConsommationCompany").put("company_name", field.getText());
-					energyField.setText(option.energyCompany());
-				}else {
-					companyField.setText("cancel");
+//					Client.map.get("energyConsommationCompany").put("company_name", field.getText());
+//					energyField.setText(option.energyCompany());
+					Client.map.get("usedBatiment").put("company_name", field.getText());
+					batimentField.setText(option.usedBatiments());
+				}else if (response == JOptionPane.CANCEL_OPTION) {
+					titleField.setText("vous n'avez pas choisi d'entreprise");
+					companyField.removeAll();
+					itemsField.removeAll();
+					equipmentField.removeAll();
+					sensorField.removeAll();
+					energyField.removeAll();
+					occupationField.removeAll();
+					batimentField.removeAll();
+
 				}
 			}
 
 
 		});
 		topPanel.add(infoByCompany);
-		JButton infoByBuilding = new JButton("indicateur par batiments");
+		infoByBuilding = new JButton("indicateur par batiments");
 		infoByBuilding.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				JPanel pan = new JPanel(new BorderLayout());
@@ -132,8 +153,8 @@ public class Indicator extends JFrame {
 				comboBox.addActionListener(new ActionListener() {
 					@Override
 					public void actionPerformed(ActionEvent e) {
+
 						field.setText(comboBox.getSelectedItem().toString());
-						titleField.setText("vous êtes sur les informations génerales du bâtiment "+ field.getText());
 					}
 				});
 
@@ -146,25 +167,32 @@ public class Indicator extends JFrame {
 				pan.add(field, BorderLayout.CENTER);
 
 				int response = JOptionPane.showConfirmDialog(null, pan, "choix d'une entreprise", JOptionPane.OK_CANCEL_OPTION);
-				//log.info("deuxieme "+field.getText());
-				if(response == JOptionPane.OK_OPTION){
 
+				if(response == JOptionPane.OK_OPTION){
+					titleField.setText("vous êtes sur les informations génerales du bâtiment: " +field.getText());
 //					Client.map.get("rateBuilding").put("building_name", field.getText());
 //					occupationField.setText(option.buildingRate());
-//					Client.map.get("objectBuilding").put("building_name", field.getText());
-//					itemsField.setText(option.objectBuilding());
-//					Client.map.get("equipmentBuilding").put("building_name", field.getText());
-//					equipmentField.setText(option.equipmentBuilding());
-//					Client.map.get("sensorBuilding").put("building_name", field.getText());
-//					sensorField.setText(option.sensorBuilding());
+					Client.map.get("objectBuilding").put("building_name", field.getText());
+					itemsField.setText(option.objectBuilding());
+					Client.map.get("equipmentBuilding").put("building_name", field.getText());
+					equipmentField.setText(option.equipmentBuilding());
 
-					log.info("le client "+Client.map.get("energyBuilding").put("building_name", field.getText()));
+					Client.map.get("sensorBuilding").put("building_name", field.getText());
+					sensorField.setText(option.sensorBuilding());
+					Client.map.get("companyBuilding").put("building_name", field.getText());
+					companyField.setText(option.companyInBuilding());
 					Client.map.get("energyBuilding").put("building_name", field.getText());
-					log.info("je vois " + field.getText());
 					energyField.setText(option.energyBuilding());
-				}
-				{
-					companyField.setText("cancel");
+					Client.map.get("allFloor").put("building_name", field.getText());
+					floorField.setText(option.allFloor());
+				}else {
+					titleField.setText("vous n'avez pas choisi de bâtiments");
+					companyField.removeAll();
+					itemsField.removeAll();
+					equipmentField.removeAll();
+					sensorField.removeAll();
+					energyField.removeAll();
+					occupationField.removeAll();
 				}
 			}
 			});
@@ -187,13 +215,8 @@ public class Indicator extends JFrame {
 
 	private JPanel optionOFCentered() {
 		JPanel mainPanel = new JPanel(new FlowLayout());
-
-
-
-
-
 		//set in a panel all the list of indicators
-		JPanel indicatorPanel = new JPanel(new GridLayout(6,1));
+		JPanel indicatorPanel = new JPanel(new GridLayout(8,1));
 		indicatorPanel.setPreferredSize(new Dimension(200, 200));
 		JLabel occupation = new JLabel("Taux d'occupation");
 		indicatorPanel.add(occupation);
@@ -207,12 +230,16 @@ public class Indicator extends JFrame {
 		indicatorPanel.add(company);
 		JLabel energy = new JLabel("Consommation énergétique");
 		indicatorPanel.add(energy);
+		JLabel floorPanel = new JLabel("Nombre d'étages");
+		indicatorPanel.add(floorPanel);
+		JLabel batimentPanel = new JLabel("Batiment utilisé");
+		indicatorPanel.add(batimentPanel);
 		mainPanel.add(indicatorPanel);
 
 /********** set in a panel all the list of each indicators result*****************/
 
 
-		JPanel fieldPanel = new JPanel(new GridLayout(6,1));
+		JPanel fieldPanel = new JPanel(new GridLayout(8,1));
 		fieldPanel.setPreferredSize(new Dimension(100, 200));
 		fieldPanel.add(occupationField);
 		fieldPanel.add(itemsField);
@@ -220,6 +247,8 @@ public class Indicator extends JFrame {
 		fieldPanel.add(sensorField);
 		fieldPanel.add(companyField);
 		fieldPanel.add(energyField);
+		fieldPanel.add(floorField);
+		fieldPanel.add(batimentField);
 		mainPanel.add(fieldPanel);
 		return mainPanel;
 	}
